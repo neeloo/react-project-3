c3cc016f0036c08a8d87c4bfb5f7dbb64e9b8871
Nilu patel <nilusingh945164@gmail.com>
2023-08-04 12:35:48 +0530

    project commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React project
+          Learn React projects
         </a>
       </header>
     </div>
