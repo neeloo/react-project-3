1204704e0295af87543d3af3f6e50d1c1a876169
Nilu patel <nilusingh945164@gmail.com>
2023-08-06 22:03:41 +0530

    project1

---
+++ src/Component/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './App.css';
+import Header from './Header.js';
+//import ContectList from './ContectList.js';
+import AddContect from './AddContent.js';
+
+function App() {
+  return (
+    <div>
+      <Header />
+       <AddContect />
+      {/* <ContectList />  */}
+    </div>
+  )
+}
+
+export default App
