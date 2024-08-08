66b8fdffcc47c34351fc12ec974de89e63103a04
Nilu patel <nilusingh945164@gmail.com>
2024-08-08 20:39:36 +0530

    js

--- src/Component/Header.js
+++ src/Component/Header.js
@@ -2,9 +2,9 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div className="ui fixed menu">
+    <div className="ui fixed menu" style={{ backgroundColor: "blue",color:"white" }}>
       <div className="ui container center">
-        <h2>Contact Manager</h2>
+        <h2>Contact Manager </h2>
       </div>
     </div>
   );
